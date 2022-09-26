68f74d4553785f341a1aaf8451d577e666770d42
jun6292 <c68254@gmail.com>
2022-09-27 00:38:09 +0900

    CpmAlg1-2up

---
+++ week1/ComAlg1-2up.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 10000;
+
+int main()
+{
+    int i, j, a[N + 1];
+    int cnt = 0;    // 곱셉 연산 횟수 구하기
+    int max_prime = 0;
+
+    for (a[1] = 0, i = 2; i <= N; i++)
+        a[i] = 1;
+    for (i = 2; i * i <= N; i++) {
+        cnt++;
+        if (a[i]) {
+            for (j = i * i; j <= N; j += i) {
+                cnt++;
+                a[j] = 0;
+            }
+        }
+    }
+    for (i = 1; i <= N; i++) {
+        if (a[i]) {
+            cout << i << ' ';
+            max_prime = i;
+        }
+    }
+    cout << "\n";
+    cout << "N 보다 작은 최대 소수: " << max_prime << "\n";
+    cout << "곱셉 연산 횟수: " << cnt << "\n";
+}
